207e8f6d85320abd0968feb7b094c82d131e0405
Sébastien LEONCE <sebastienleonce91@gmail.com>
2022-02-15 08:59:38 +0100

    create session router

---
+++ router/session.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { username, password } = req.body;
+    
+    res.json({
+        username,
+        password
+    })
+});
+
+router.delete('/', async (req, res) => {
+    const { token } = req.body;
+
+    res.json({
+        token
+    });
+});
