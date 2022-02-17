4f927f03f1dcfd5820171df70d2608f4fc9d0b02
alexisdacosta <pro.alexisdacosta@gmail.com>
2022-02-17 09:41:55 +0100

    Rollback isAuthenticated

--- middlewares/isAuthenticated.ts
+++ middlewares/isAuthenticated.ts
@@ -1,30 +1,20 @@
 import jwtUtils from 'utils/jwt-utils'
+import { User } from '@models/User'
 import { 
     Request,
     Response,
     NextFunction
 } from 'express'
-import * as MiddlewareError from 'types/error/MiddlewareError'
 
 export default async (req: Request, _res : Response, next: NextFunction) => {
-    const jwt = req.headers.cookie?.slice(4)
+    const jwt = req.signedCookies.jwt
 
     if (jwt) {
-        const user = await jwtUtils.decode(jwt);
+        const user = await jwtUtils.decode(jwt) as User
 
         if (user) {
             return next()
         }
     }
-    _res.status(401).send(MiddlewareError.UNAUTHORIZED_ERROR)
-    next()
-}
-
-export async function whoami(req: Request, _res : Response) {
-    const jwt = req.headers.cookie?.slice(4)
-
-    if (jwt) {
-        const user = await jwtUtils.decode(jwt);
-        return user
-    }
+    next('Unauthorized')
 }
