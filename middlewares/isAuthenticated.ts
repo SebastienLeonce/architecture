b9d5dcd410dd6571aae3867ddab3637f0b4da2f5
alexisdacosta <pro.alexisdacosta@gmail.com>
2022-02-17 08:47:20 +0100

    Add of whoami function to get the user JWT token and error handle when the user is unauthorized

--- middlewares/isAuthenticated.ts
+++ middlewares/isAuthenticated.ts
@@ -4,6 +4,7 @@ import {
     Response,
     NextFunction
 } from 'express'
+import * as MiddlewareError from 'types/error/MiddlewareError'
 
 export default async (req: Request, _res : Response, next: NextFunction) => {
     const jwt = req.headers.cookie?.slice(4)
@@ -15,5 +16,15 @@ export default async (req: Request, _res : Response, next: NextFunction) => {
             return next()
         }
     }
-    next('Unauthorized')
+    _res.status(401).send(MiddlewareError.UNAUTHORIZED_ERROR)
+    next()
+}
+
+export async function whoami(req: Request, _res : Response) {
+    const jwt = req.headers.cookie?.slice(4)
+
+    if (jwt) {
+        const user = await jwtUtils.decode(jwt);
+        return user
+    }
 }
