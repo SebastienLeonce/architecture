7917cb97eceab9cce7b75d5c34307e286de78c5e
Sébastien LEONCE <sebastienleonce91@gmail.com>
2022-02-18 14:29:29 +0100

    add example to swagger

--- docs/components.ts
+++ docs/components.ts
@@ -2,16 +2,21 @@ import m2s            from 'mongoose-to-swagger';
 
 import { UserModel } from "@models/User";
 
-const newUser = m2s(UserModel);
+const options = {
+    props: ['example'],
+}
+
+const newUser = m2s(UserModel, options);
 delete newUser.properties._id
 delete newUser.properties.admin
 newUser.title = "newUser"
 
-const getUser = m2s(UserModel);
+const getUser = m2s(UserModel, options);
 delete getUser.properties.password
+delete getUser.properties.admin
 getUser.title = "getUser"
 
-const login = m2s(UserModel);
+const login = m2s(UserModel, options);
 delete login.properties._id
 delete login.properties.admin
 delete login.properties.mail
@@ -20,7 +25,7 @@ login.title = "login"
 export default {
     components: {
         schemas: {
-            User: m2s(UserModel),
+            User: m2s(UserModel, options),
             newUser,
             getUser,
             login
