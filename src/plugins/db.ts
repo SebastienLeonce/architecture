fbd266ef85572f003bc0a540935eb80eebfe667a
Sébastien LEONCE <sebastien.leonce@cea.fr>
2022-02-20 14:15:55 +0100

    add mongodb to service

--- src/plugins/db.ts
+++ src/plugins/db.ts
@@ -1,17 +1,11 @@
 import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 
-MongoMemoryServer.create().then(mongoServer => {
-    const DB_URL = process.env.NODE_ENV == 'prod' ? process.env.DB_URL : mongoServer.getUri();
-
-    mongoose.connect(DB_URL as string)
-        .then(async () => {
-            // console.log("MongoDB Connected...");
-        }).catch((error) => {
-            console.error('connection error:', error);
-            process.exit(1);
-        });
-});
-
+mongoose.connect('mongodb://mongo-db:27017')
+    .then(async () => {
+        // console.log("MongoDB Connected...");
+    }).catch((error) => {
+        console.error('connection error:', error);
+        process.exit(1);
+    });
 
 export default {}
