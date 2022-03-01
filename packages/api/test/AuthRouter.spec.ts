b1da38841728f57a8a150848a3db63d49cdec6a7
Sebastien Leonce <sebastienleonce91@gmail.com>
2022-03-01 14:57:12 +0100

    add new test

--- packages/api/test/AuthRouter.spec.ts
+++ packages/api/test/AuthRouter.spec.ts
@@ -3,6 +3,8 @@ import { generateUser } from './fixtures/generateUser';
 
 import app from '../src/index'
 import { UserModel } from '@models/User'
+import * as AuthError from "@shared/error/AuthError";
+import * as RequestError from "@shared/error/RequestError";
 
 describe('AuthenticationRouter', () => {
     const user = generateUser();
@@ -37,13 +39,52 @@ describe('AuthenticationRouter', () => {
                 })
         })
 
+        it("Login with wrong password", (done) => {
+            const user2 = user;
+            user2.password = "WrongPassword"
+
+            agent
+                .post("/api/auth/login")
+                .send(user2)
+                .expect(401)
+                .expect(AuthError.USER_PASSWORD_ERROR)
+                .end((err, _res) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
+
+        it("Login with empty body", (done) => {
+          agent
+            .post("/api/auth/login")
+            .send({})
+            .expect(400)
+            .expect(RequestError.BODY_PARAMETERS_UNDEFINED_ERROR)
+            .end((err, _res) => {
+                if (err) return done(err);
+                done();
+            });
+        });
+
+        it("Register with empty body", (done) => {
+          agent
+            .post("/api/auth/register")
+            .send({})
+            .expect(400)
+            .expect(RequestError.BODY_PARAMETERS_UNDEFINED_ERROR)
+            .end((err, _res) => {
+                if (err) return done(err);
+                done();
+            });
+        });
+
         it('Shoud Destroy JWT', done => {
-            agent.
-                delete('/api/session').
-                expect(200).
-                end((_err, _res) => {
-                    done()
-                })
+            agent
+              .delete("/api/auth/logout")
+              .expect(200)
+              .end((_err, _res) => {
+                done();
+              });
         })
     });
 });
